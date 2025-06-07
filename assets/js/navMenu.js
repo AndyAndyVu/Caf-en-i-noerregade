554a7586b6ad0d1c6234c68d7ba373e71ebca5b2
Huy Vu <huy_htv@hotmail.com>
2025-06-07 13:01:31 +0200

    Small changes and blur on active menu
    added blur on menu open and changes on for the nav design etc.

--- assets/js/navMenu.js
+++ assets/js/navMenu.js
@@ -1,6 +1,8 @@
-const navMenuButton = document.querySelector('.menuButton');
-const menuContainer = document.querySelector('.menuContainer');
+const navMenuButton = document.querySelector(".menuButton");
+const menuContainer = document.querySelector(".menuContainer");
+const menuBlur = document.querySelector(".menuOnActiveBlur");
 
-navMenuButton.addEventListener('click', () => {
-    menuContainer.classList.toggle('menuContainerActive');
-})
+navMenuButton.addEventListener("click", () => {
+  menuContainer.classList.toggle("menuContainerActive");
+  menuBlur.classList.toggle("blurActive");
+});
