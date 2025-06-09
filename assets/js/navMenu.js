cf9b05e3eb656b70eb91096888ff33c143edddd0
Huy Vu <huy_htv@hotmail.com>
2025-06-09 05:30:01 +0200

    Javascript fetch af api'er
    MadItems og MadKategori fatched og displayed og layouted i css.
    Mangler konventere tags til ikoner., atm er de strings.

--- assets/js/navMenu.js
+++ assets/js/navMenu.js
@@ -2,6 +2,7 @@ const navMenuButton = document.querySelector(".menuButton");
 const menuContainer = document.querySelector(".menuContainer");
 const menuBlur = document.querySelector(".menuOnActiveBlur");
 
+
 navMenuButton.addEventListener("click", () => {
   menuContainer.classList.toggle("menuContainerActive");
   menuBlur.classList.toggle("blurActive");
