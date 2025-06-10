caa1cf96273031e9c45ccd2d1ae79f7b5a6121df
AndyAndyVu <andyandyvu@gmail.com>
2025-06-10 14:31:03 +0200

    Update blog.js

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -1,17 +1,3 @@
-// const domain = "https://www.huyvu.dk/";
-// const postsEndpointBlog = "wp-json/wp/v2/blog-post"; 
-
-// // Kald API og log resultaterne
-// async function fetchBlogs() {
-//   try {
-//     const response = await fetch(domain + postsEndpointBlog);
-//     const Blogindlæg = await response.json();
-//     console.log(" Blog-indlæg hentet:", Blogindlæg);
-//   } catch (error) {
-//     console.error("Fejl ved hentning af blogs:", error);
-//   }
-// }
-
 const domain = "https://www.huyvu.dk/";
 const postsEndpointBlog = "wp-json/wp/v2/blog-post"; 
 const getRealImageUrls = "?acf_format=standard";
@@ -26,7 +12,6 @@ async function fetchBlogs() {
 
     console.log("Blog-indlæg hentet:", blogIndlaeg);
 
-    // Indsæt indlæggene i DOM
     blogIndlaeg.forEach((post) => {
       blogContainer.innerHTML += `
         <article class="blog-post">
