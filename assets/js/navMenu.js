efeaf4d408ccddc2f38d5a796a262ee8f9a9b787
Huy Vu <huy_htv@hotmail.com>
2025-06-10 17:59:39 +0200

    Small fixes
    Fixed spacing of åbningstider
    Fixed menu buttons toggle being under the active blur. with < index.

--- assets/js/navMenu.js
+++ assets/js/navMenu.js
@@ -1,9 +1,10 @@
 const navMenuButton = document.querySelector(".menuButton");
 const menuContainer = document.querySelector(".menuContainer");
 const menuBlur = document.querySelector(".menuOnActiveBlur");
+const menuButtons = document.querySelector(".menu-toggle");
 
 navMenuButton.addEventListener("click", () => {
   menuContainer.classList.toggle("menuContainerActive");
   menuBlur.classList.toggle("blurActive");
+  menuButtons.classList.toggle("menu-toggleZIndex");
 });
-
