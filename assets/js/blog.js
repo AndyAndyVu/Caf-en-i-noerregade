8f68021b4056f243463acf35792b677ea9437a5f
AndyAndyVu <andyandyvu@gmail.com>
2025-06-10 14:27:11 +0200

    b

--- assets/js/blog.js
+++ assets/js/blog.js
@@ -14,12 +14,14 @@
 
 const domain = "https://www.huyvu.dk/";
 const postsEndpointBlog = "wp-json/wp/v2/blog-post"; 
+const getRealImageUrls = "?acf_format=standard";
+
 
 const blogContainer = document.getElementById("blog-container");
 
 async function fetchBlogs() {
   try {
-    const response = await fetch(domain + postsEndpointBlog);
+    const response = await fetch(domain + postsEndpointBlog + getRealImageUrls);
     const blogIndlaeg = await response.json();
 
     console.log("Blog-indlæg hentet:", blogIndlaeg);
@@ -28,8 +30,10 @@ async function fetchBlogs() {
     blogIndlaeg.forEach((post) => {
       blogContainer.innerHTML += `
         <article class="blog-post">
-          <h2>${post.title.rendered}</h2>
-          <div>${post.content.rendered}</div>
+            <p>${post.acf["kategori-label"]} </p>
+            <h2>${post.title.rendered}</h2>
+            <div>${post.content.rendered}</div>
+            <img src="${post.acf.billede.sizes.medium}" alt="">
         </article>
       `;
     });
